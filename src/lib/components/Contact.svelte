89254e8b9c6b149ead745276b7fea490907eee84
Chris <chrissolanilla@gmail.com>
2024-05-27 03:15:35 -0400

    added logo and fixed hero section and footer

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -1,24 +1,38 @@
 <div class="ContactContainer">
     <div class="ContactTitle">
         <h1>Contact us</h1>
+        <div class="ContactBody">
+            <div class="Address">
+                10145 NW 19th Street Doral, FL 33172
+            </div>
+            <div class="Phone">
+                <b>Phone:</b>
+                866-88-BURKE
+            </div>
+            <div class="Fax">
+                <b>Fax:</b>
+                (305) 468-6654
+            </div>
+            <div class="Email">
+                <b>Email:</b>
+                <a href="mailto:info@bcginc.net">info@bcginc.net</a>
+            </div>
+        </div>
     </div>
-    <div class="ContactBody">
-        <div class="Address">
-            10145 NW 19th Street Doral, FL 33172
-        </div>
-        <div class="Phone">
-            <b>Phone:</b>
-            866-88-BURKE
-        </div>
-        <div class="Fax">
-            <b>Fax:</b>
-            (305) 468-6654
-        </div>
-        <div class="Email">
-            <b>Email:</b>
-            <a href="mailto:info@bcginc.net">info@bcginc.net</a>
-        </div>
-    </div>
+    <div class="icons">
+		<a href="https://www.linkedin.com/company/burkeconstructiongroup">
+			<i class="fa fa-linkedin" aria-hidden="true"></i>
+		</a>
+		<a href="https://www.linkedin.com/company/burkeconstructiongroup"> 
+			<i class="fa fa-instagram" aria-hidden="true"></i>
+		</a>
+		<a href="https://www.facebook.com/BurkeConstructionGroupInc/">
+			<i class="fa fa-facebook" aria-hidden="true"></i>
+		</a>
+		<a href="https://www.youtube.com/channel/UCvopoFV385wOPjFQPDb6k5A">
+			<i class="fa fa-youtube" aria-hidden="true"></i>
+		</a>
+	</div>
 </div>
 
 <style>
@@ -26,12 +40,23 @@
         width: 100rem;
         height: auto;
         display: flex;
-        flex-direction: column;
+        flex-direction: row;
         justify-content: center;
         align-items: center;
-        gap: 4px;
+        column-gap: 50rem;
+        padding: 0.5rem 0 2rem 0;
     }
     .ContactBody{
         gap: 5px;
     }
+    .icons {    
+		display:flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
+
+	i {
+		padding:0.4rem;
+	}
 </style>
