f8e357ce4429ae3fcd1b7075b49fba6c09f6f4b7
Chris <chrissolanilla@gmail.com>
2024-05-27 22:40:34 -0400

    fixes mobile view of team

--- src/lib/components/Team.svelte
+++ src/lib/components/Team.svelte
@@ -1,187 +1,216 @@
 <div class="TeamContainer">
-    <div class="TeamTitle">
-        <h1>Team</h1>
-    </div>
-    <div class="TeamBios">
-        <div class="Anthony">
-            <div class="Bio">
-                <h3>Anthony J. Burke</h3>
-                <h4><i>Chief Executive Officer</i></h4>
-                <p> Mr. Burke is one of the original founding partners of Burke Construction Group. 
-                    He has served as the CEO since 1997. Mr. Burke's primary focus is on developing 
-                    and maintaining client relations. He works closely on the preconstruction services, 
-                    preparation of project estimates and oversees contract negotiations.
-                </p>
-                <p>
-                    Mr. Burke started his career in the construction industry as a field engineer over thirty years ago. 
-                    Prior to founding Burke Construction, Mr. Burke was the founder of The Tower Group which he owned 
-                    and managed for twelve years prior to selling the firm to an international company. Under Burke's 
-                    leadership Tower grew to over 250 employees and 185 million in annual revenues. Prior to founding 
-                    the Tower Group, Burke worked for ten years with a prominent local Florida developer serving as 
-                    President of their Construction Division where he designed and constructed more than 4.2 million 
-                    square feet of commercial space. Mr. Burke graduated from Indiana State University with a B.S. in 
-                    Construction Management in 1982. In addition to his leadership role with the entities stated above, 
-                    he was appointed by Governor Jeb Bush to serve on the State of Florida's Construction Industry 
-                    Licensing Board and presently serves as an advisor to his church board. Mr. Burke is a State Certified 
-                    General Contractor.
-                </p>
-            </div>
-            <div class="AnthonyImg">
-                <img src="Anthony-J.-Burke-2018.jpg" alt="Chief Executive Officer, Anthony J Burke">
-            </div>
-        </div>
-        
-        <div class="David">
-            <div class="BioReverse">
-                <h3>David Martinez</h3>
-                <h4><i>Chief Financial Officer</i></h4>
-                <p>
-                    David W. Martinez began his career twenty-eight years ago as a staff accountant and 
-                    local municipality's auditor. In 1987, Mr. Martinez joined the CPA firm of Muskat, 
-                    Simon & Kolber, one of South Florida's top 50 accounting firms. There, he worked his 
-                    way up from staff accountant to senior staff manager and in 1992 he opened his own CPA 
-                    firm. His experience includes tax, financial, audit and consulting services, with a heavy 
-                    concentration in the construction industry. Mr. Martinez has also been highly recognized 
-                    as an expert witness for numerous court cases dealing with multi-million dollar claims. 
-                    His knowledge of management, accounting and financial matters enabled him to detect, 
-                    calculate and report on several frauds and mismanagement claims.
-                </p>
-                <p>
-                    In September 2000 Mr. Martinez became Chief Financial Officer and Partner of The Tower Group, 
-                    Inc. He was responsible for overseeing the accounting staff, securing adequate bonding, 
-                    insurance, and credit, as well as, controlling the overall financial stability of the 
-                    corporation. He works closely on the preparation of all financial reports, project budgets, 
-                    managing of assets and the implementation of the internal control and computer systems. In 2007, 
-                    Mr. Martinez, along with Anthony Burke, launched Burke Construction Group, Inc. Together these 
-                    two have completed over $1.7 billion of construction projects throughout the state of Florida in 
-                    just the last 17 years. Education: University of Central Florida, Associates Degree Florida 
-                    International University Miami, FL - 1987 Bachelor of Accounting, College of Business Administration 
-                    Registrations: Certified Public Accountant, State of Florida Member of the Florida Institute of 
-                    Certified Public Accountants Member of the American Institute of Certified Public Accountants Member 
-                    of the Construction Financial Management Association, Member of the FIU Construction Industry 
-                    Advisory Board and University of Miami Master of Construction Management Advisory Board.
-                </p>
-            </div>
-            <div class="DavidImg">
-                <img src="David-W-M.jpg" alt="Chief Financial Officer, David Martinez">
-            </div>
-        </div>
-        
-        <div class="Steve">
-            <div class="Bio">
-                <h3>Steve Alessi</h3>
-                <h4><i>Temp Title</i></h4>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Proin eleifend sem eget felis bibendum maximus. Vestibulum ullamcorper eu quam vel 
-                    lacinia. Nunc imperdiet fermentum urna. Cras vitae condimentum enim. 
-                    Fusce urna est, cursus vehicula nisi sed, cursus rhoncus erat. Pellentesque sed suscipit ipsum, 
-                    sit amet efficitur dolor. Donec dictum eu ante ut sollicitudin. Maecenas lobortis vitae erat quis pellentesque. 
-                    Sed at augue quis est condimentum elementum in sed sem. Cras eu congue ligula. Nulla fermentum est id augue scelerisque, 
-                    at scelerisque nisl tincidunt.
-                </p>
-                <p>
-                    Morbi non dapibus nisi, vel congue magna. Vivamus tincidunt dictum ante ut auctor. Proin et magna sit amet odio tristique 
-                    sollicitudin vitae ut ante. Donec mattis elementum convallis. Nunc ac leo enim. Duis bibendum posuere ipsum in venenatis. 
-                    Fusce varius, arcu ac sollicitudin molestie, magna arcu cursus arcu, vel lacinia magna ligula ut sem. Donec in consectetur 
-                    neque. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-                    Duis sit amet metus in felis accumsan blandit in sit amet nibh. Curabitur sed dictum felis. Morbi ac mattis massa, vel 
-                    malesuada est. Ut blandit vel tortor at sagittis. Vivamus scelerisque magna sit amet diam malesuada aliquam vel 
-                    sollicitudin eros. Fusce sit amet feugiat ipsum. Quisque consequat mi ut elit maximus, at aliquet lacus gravida. Donec quis 
-                    est purus. Etiam semper turpis id turpis hendrerit, non vehicula velit dictum.
-                </p>
-            </div>
-            <div class="SteveImg">
-                <img src="Steve.jpg" alt="Temp Title, Steve Alessi">
-            </div>
-        </div>
-    </div>
+	<div class="TeamTitle">
+		<h1>Team</h1>
+	</div>
+	<div class="TeamBios">
+		<div class="Anthony">
+			<div class="Bio">
+				<h3>Anthony J. Burke</h3>
+				<h4><i>Chief Executive Officer</i></h4>
+				<p> Mr. Burke is one of the original founding partners of Burke Construction Group. 
+					He has served as the CEO since 1997. Mr. Burke's primary focus is on developing 
+					and maintaining client relations. He works closely on the preconstruction services, 
+					preparation of project estimates and oversees contract negotiations.
+				</p>
+				<p>
+					Mr. Burke started his career in the construction industry as a field engineer over thirty years ago. 
+					Prior to founding Burke Construction, Mr. Burke was the founder of The Tower Group which he owned 
+					and managed for twelve years prior to selling the firm to an international company. Under Burke's 
+					leadership Tower grew to over 250 employees and 185 million in annual revenues. Prior to founding 
+					the Tower Group, Burke worked for ten years with a prominent local Florida developer serving as 
+					President of their Construction Division where he designed and constructed more than 4.2 million 
+					square feet of commercial space. Mr. Burke graduated from Indiana State University with a B.S. in 
+					Construction Management in 1982. In addition to his leadership role with the entities stated above, 
+					he was appointed by Governor Jeb Bush to serve on the State of Florida's Construction Industry 
+					Licensing Board and presently serves as an advisor to his church board. Mr. Burke is a State Certified 
+					General Contractor.
+				</p>
+			</div>
+			<div class="AnthonyImg">
+				<img src="Anthony-J.-Burke-2018.jpg" alt="Chief Executive Officer, Anthony J Burke">
+			</div>
+		</div>
+		
+		<div class="David">
+			<div class="BioReverse">
+				<h3>David Martinez</h3>
+				<h4><i>Chief Financial Officer</i></h4>
+				<p>
+					David W. Martinez began his career twenty-eight years ago as a staff accountant and 
+					local municipality's auditor. In 1987, Mr. Martinez joined the CPA firm of Muskat, 
+					Simon & Kolber, one of South Florida's top 50 accounting firms. There, he worked his 
+					way up from staff accountant to senior staff manager and in 1992 he opened his own CPA 
+					firm. His experience includes tax, financial, audit and consulting services, with a heavy 
+					concentration in the construction industry. Mr. Martinez has also been highly recognized 
+					as an expert witness for numerous court cases dealing with multi-million dollar claims. 
+					His knowledge of management, accounting and financial matters enabled him to detect, 
+					calculate and report on several frauds and mismanagement claims.
+				</p>
+				<p>
+					In September 2000 Mr. Martinez became Chief Financial Officer and Partner of The Tower Group, 
+					Inc. He was responsible for overseeing the accounting staff, securing adequate bonding, 
+					insurance, and credit, as well as, controlling the overall financial stability of the 
+					corporation. He works closely on the preparation of all financial reports, project budgets, 
+					managing of assets and the implementation of the internal control and computer systems. In 2007, 
+					Mr. Martinez, along with Anthony Burke, launched Burke Construction Group, Inc. Together these 
+					two have completed over $1.7 billion of construction projects throughout the state of Florida in 
+					just the last 17 years. Education: University of Central Florida, Associates Degree Florida 
+					International University Miami, FL - 1987 Bachelor of Accounting, College of Business Administration 
+					Registrations: Certified Public Accountant, State of Florida Member of the Florida Institute of 
+					Certified Public Accountants Member of the American Institute of Certified Public Accountants Member 
+					of the Construction Financial Management Association, Member of the FIU Construction Industry 
+					Advisory Board and University of Miami Master of Construction Management Advisory Board.
+				</p>
+			</div>
+			<div class="DavidImg">
+				<img src="David-W-M.jpg" alt="Chief Financial Officer, David Martinez">
+			</div>
+		</div>
+		
+		<div class="Steve">
+			<div class="Bio">
+				<h3>Steve Alessi</h3>
+				<h4><i>Temp Title</i></h4>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+					Proin eleifend sem eget felis bibendum maximus. Vestibulum ullamcorper eu quam vel 
+					lacinia. Nunc imperdiet fermentum urna. Cras vitae condimentum enim. 
+					Fusce urna est, cursus vehicula nisi sed, cursus rhoncus erat. Pellentesque sed suscipit ipsum, 
+					sit amet efficitur dolor. Donec dictum eu ante ut sollicitudin. Maecenas lobortis vitae erat quis pellentesque. 
+					Sed at augue quis est condimentum elementum in sed sem. Cras eu congue ligula. Nulla fermentum est id augue scelerisque, 
+					at scelerisque nisl tincidunt.
+				</p>
+				<p>
+					Morbi non dapibus nisi, vel congue magna. Vivamus tincidunt dictum ante ut auctor. Proin et magna sit amet odio tristique 
+					sollicitudin vitae ut ante. Donec mattis elementum convallis. Nunc ac leo enim. Duis bibendum posuere ipsum in venenatis. 
+					Fusce varius, arcu ac sollicitudin molestie, magna arcu cursus arcu, vel lacinia magna ligula ut sem. Donec in consectetur 
+					neque. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+					Duis sit amet metus in felis accumsan blandit in sit amet nibh. Curabitur sed dictum felis. Morbi ac mattis massa, vel 
+					malesuada est. Ut blandit vel tortor at sagittis. Vivamus scelerisque magna sit amet diam malesuada aliquam vel 
+					sollicitudin eros. Fusce sit amet feugiat ipsum. Quisque consequat mi ut elit maximus, at aliquet lacus gravida. Donec quis 
+					est purus. Etiam semper turpis id turpis hendrerit, non vehicula velit dictum.
+				</p>
+			</div>
+			<div class="SteveImg">
+				<img src="Steve.jpg" alt="Temp Title, Steve Alessi">
+			</div>
+		</div>
+	</div>
 </div>
 
 <style>
-    .TeamContainer{
-        color: white;
-        width: 100rem;
-        height: auto;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        gap: 4px;
-        padding: 1rem;
-    }
-    .TeamBios{
-        width: 100rem;
-        height: auto;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        gap: 50px;
-    }
-    .Anthony{
-        background: linear-gradient(89.2deg, rgb(119, 217, 211) 5.3%, rgb(5, 120, 190) 94.7%);
-        border-radius: 1rem;
-        padding: 1rem;
-        display: flex;
-        flex-direction: row-reverse;
-        justify-content: center;
-        align-items: center;
-        gap: 4px;
-    }
-    .Bio{
-        width: auto;
-        margin: 0 auto;
-        font-size: 20px;
-        padding-left: 50px;
-        display: block;
-        position: relative;
-    }
-    .BioReverse{
-        width: auto;
-        margin: 0 auto;
-        font-size: 20px;
-        padding-right: 50px;
-        display: block;
-        position: relative;
-    }
-    .AnthonyImg{
-        border-radius: 1rem;
-    }
-    .AnthonyImg img{
-        border-radius: 1rem;
-    }
-    .David{
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-        gap: 4px;
-        background: linear-gradient(-225deg, #3D4E81 0%, #5753C9 48%, #6E7FF3 100%);
-        padding: 1rem;
-        border-radius: 1rem;
-    }
-    .DavidImg{
-        border-radius: 1rem;
-    }
-    .DavidImg img{
-        border-radius: 1rem;
-    }
-    .Steve{
-        display: flex;
-        flex-direction: row-reverse;
-        justify-content: center;
-        align-items: center;
-        gap: 4px;
-        background: linear-gradient(to right, #243949 0%, #517fa4 100%);
-        padding: 1rem;  
-        border-radius: 1rem;
-    }
-    .SteveImg img{
-        height: 500px;
-        border-radius: 1rem;
-    }
-    .SteveImg{
-        border-radius: 1rem;
-    }
+	.TeamContainer {
+		color: white;
+		width: 100%;
+		max-width: 100rem;
+		height: auto;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 4px;
+		padding: 1rem;
+	}
+
+	.TeamTitle {
+		margin-bottom: 2rem;
+	}
+
+	.TeamBios {
+		width: 100%;
+		max-width: 100rem;
+		height: auto;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 50px;
+	}
+
+	.Anthony,
+	.David,
+	.Steve {
+		border-radius: 1rem;
+		padding: 1rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 4px;
+		flex-direction: row-reverse;
+	}
+
+	.Anthony {
+		background: linear-gradient(89.2deg, rgb(119, 217, 211) 5.3%, rgb(5, 120, 190) 94.7%);
+	}
+
+	.David {
+		background: linear-gradient(-225deg, #3D4E81 0%, #5753C9 48%, #6E7FF3 100%);
+	}
+
+	.Steve {
+		background: linear-gradient(to right, #243949 0%, #517fa4 100%);
+	}
+
+	.Bio,
+	.BioReverse {
+		width: 100%;
+		font-size: 20px;
+		display: block;
+		position: relative;
+		padding: 0 20px;
+	}
+
+	.Bio h3,
+	.BioReverse h3 {
+		margin-top: 0;
+	}
+
+	.AnthonyImg,
+	.DavidImg,
+	.SteveImg {
+		border-radius: 1rem;
+		flex: 0 0 150px;
+		max-width: 150px;
+	}
+
+	.AnthonyImg img,
+	.DavidImg img,
+	.SteveImg img {
+		border-radius: 1rem;
+		width: 100%;
+		height: auto;
+	}
+
+	@media (max-width: 900px) {
+		.TeamBios {
+			gap: 20px;
+		}
+		
+		.Anthony,
+		.David,
+		.Steve {
+			flex-direction: column-reverse;
+			text-align: center;
+			padding: 1rem;
+		}
+		
+		.Bio,
+		.BioReverse {
+			padding: 0;
+		}
+		
+		.AnthonyImg,
+		.DavidImg,
+		.SteveImg {
+			max-width: 100%;
+			margin-bottom: 1rem;
+		}
+		
+		.Bio p,
+		.BioReverse p {
+			font-size: 1rem;
+			margin-bottom: 10px;
+		}
+	}
+
 </style>
